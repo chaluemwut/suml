a8c67c240c9af0a3f7162eb8d6d7adf065e7f330
chaluemwut <chaluemwut@gmail.com>
2015-07-12 00:13:32 +0700

    system runner#1

--- cmp_process.py
+++ cmp_process.py
@@ -8,21 +8,33 @@ from sklearn.svm import SVC
 
 from dataset_loader import DataSetLoader
 from sklearn import cross_validation
-import copy, random, time
+import copy, random, time, logging, sys, pickle
 import numpy as np
+from tabulate import tabulate
 
-ml = ['bagging', 'boosted', 'randomforest', 'nb', 'knn', 'decsiontree', 'svm']
+ml_name = ['bagging', 'boosted', 'randomforest', 'nb', 'knn', 'decsiontree', 'svm']
 
+log = logging.getLogger('')
+log.setLevel(logging.DEBUG)
+format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        
+ch = logging.StreamHandler(sys.stdout)
+ch.setFormatter(format)
+log.addHandler(ch)
+      
+fh = logging.FileHandler('data.log')
+fh.setFormatter(format)
+log.addHandler(fh)
+            
 class CompareProcess(object):
     data_size = [0.75, 0.50, 0.25]
-    reperating_loop = 10
+    reperating_loop = 50
     
     def __init__(self):
         pass
     
     def gen_ml_lst(self):
-        base_estimation = [2,4]#[2, 32, 64, 1024]
-        knn = [2, 32, 64, 1024]
+        base_estimation = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
         
         random_lst = []
         boosted_lst = []
@@ -33,8 +45,6 @@ class CompareProcess(object):
             bagging_lst.append(BaggingClassifier(DecisionTreeClassifier(), n_estimators=i))
         
         knn_lst = []
-        for i in knn:
-            knn_lst.append(KNeighborsClassifier(n_neighbors=i))
             
         svm_lst = []
         svm_lst.append(SVC(kernel='linear'))
@@ -42,14 +52,22 @@ class CompareProcess(object):
         svm_lst.append(SVC(kernel='poly', degree=3))
         svm_lst.append(SVC(kernel='sigmoid'))
         
-        return {ml[0]:bagging_lst,
-                ml[1]:boosted_lst,
-                ml[2]:random_lst,
-                ml[3]:[GaussianNB()],
-                ml[4]:knn_lst,
-                ml[5]:[DecisionTreeClassifier()],
-                ml[6]:svm_lst
+        return {
+                ml_name[0]:bagging_lst,
+                ml_name[1]:boosted_lst,
+                ml_name[2]:random_lst,
+                ml_name[3]:[GaussianNB()],
+                ml_name[4]:knn_lst,
+                ml_name[5]:[DecisionTreeClassifier()],
+                ml_name[6]:svm_lst
         }
+     
+    def gen_knn(self, max_size):
+        lst_random = random.sample(range(2, max_size), 5)
+        knn_lst = []
+        for i in lst_random:
+            knn_lst.append(KNeighborsClassifier(n_neighbors=i))
+        return knn_lst
         
     def load_dataset(self):
         loader = DataSetLoader()
@@ -59,22 +77,99 @@ class CompareProcess(object):
     def cross_validation(self, ml_lst, x, y):
         score_lst = []    
         for ml in ml_lst:
-            scores = cross_validation.cross_val_score(ml, x, y, cv=5)
+            print 'start cross val'
+            scores = cross_validation.cross_val_score(ml, x, y, cv=2)
+            print 'end cross val'
             score_lst.append(scores.mean())
         np_score = np.array(score_lst)
         max_idx = np_score.argmax()
         return ml_lst[max_idx]
     
+    def find_mean_acc(self, data_value):
+        lst_acc = []
+        for data in data_value:
+            lst_acc.append(data.acc)
+        return np.mean(lst_acc)
+    
+    def report_all(self, result):
+        lst_data = []
+        time_data = []
+        for m in ml_name:
+            all_data = result[m]
+            all_data = np.array(all_data)
+            acc25 = all_data[:, 0]
+            f1_25 = all_data[:, 1]
+            time25 = all_data[:, 2]
+            total_ins25 = all_data[:, 3]
+            acc50 = all_data[:, 4]
+            f1_50 = all_data[:, 5]
+            time50 = all_data[:, 6]
+            total_ins50 = all_data[:, 7]
+            acc75 = all_data[:, 8]
+            f1_75 = all_data[:, 9]
+            time75 = all_data[:, 10]
+            total_ins75 = all_data[:, 11]                        
+            lst_data.append([m, acc25, acc50, acc75, f1_25, f1_50, f1_75])
+            time_data.append([m, time25, total_ins25, time50, total_ins50, time75, total_ins75]) 
+        print tabulate(lst_data, headers=('ml name', 'acc 25', 'acc 50', 'acc 75', 'f1 25', 'f1 50', 'f1 75'))
+        print '----------------------------'
+        print tabulate(time_data, headers=('ml name', 'time 25', 'ins 25', 'time 50', 'ins 50', 'time 75', 'ins 75'))
+    
+    def report_by_dataset_v1(self, result):
+        print 'report by dataset'
+        result_lst = []
+        for i in range(0, len(DataSetLoader.dataset_name)):
+            print '----- data set ',DataSetLoader.dataset_name[i]
+            for m in ml_name:
+                ml_result = []
+                datasets_data = result[m][i]
+                acc25 = datasets_data[0]
+                f1_25 = datasets_data[1]
+                time25 = datasets_data[2]
+                total_ins25 = datasets_data[3]
+                acc50 = datasets_data[4]
+                f1_50 = datasets_data[5]
+                time50 = datasets_data[6]
+                total_ins50 = datasets_data[7]
+                acc75 = datasets_data[8]
+                f1_75 = datasets_data[9]
+                time75 = datasets_data[10]
+                total_ins25 = datasets_data[11]                                
+                ml_result.append(m)
+                ml_result.append(acc25)
+                ml_result.append(acc50)
+                ml_result.append(acc75)
+                ml_result.append(f1_25)
+                ml_result.append(f1_50)
+                ml_result.append(f1_75)
+                result_lst.append(ml_result)
+        print tabulate(result_lst, ('ml name','acc 25', 'acc 50', 'acc 75', 'f1 25', 'f1 50','f1 75'))
+                        
+    def report(self, result):
+        self.report_by_dataset_v1(result)
+        self.report_all(result)
+        
     def process(self):
         ml_lst = self.gen_ml_lst()
         dataset_lst = self.load_dataset()
+        result = {}
         for ml_key, ml_value in ml_lst.iteritems():
-            for data_key, data_value in dataset_lst.iteritems():
+            print '*************************************** ',ml_key
+            all_data = []           
+            for dataset_name in DataSetLoader.dataset_name:
+                data_value = dataset_lst[dataset_name]
                 x_data = data_value[0]
                 y_data = data_value[1]
+                datasets_data = []            
                 for d_size in self.data_size:
-                    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=d_size, random_state=42)
-                    ml = self.cross_validation(ml_value, x_train, y_train)
+                    ran_num = random.randint(1,100)
+                    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=d_size, random_state=ran_num)
+                    if ml_key == ml_name[4]:
+                        max_knn = int(len(x_train)/5.0)
+                        knn_lst = self.gen_knn(max_knn)
+                        ml = self.cross_validation(knn_lst, x_train, y_train)
+                    else:
+                        ml = self.cross_validation(ml_value, x_train, y_train)
                     acc_lst = []
                     f1_lst = []
                     time_pred = []
@@ -93,11 +188,28 @@ class CompareProcess(object):
                         f1_lst.append(fsc)
                         time_pred.append(total_time)
                         total_ins.append(len(y_test))
-                    print 'data size ', d_size, ' data set ', data_key, ' acc ', acc_lst
-
+                    datasets_data.append(np.mean(acc_lst))
+                    datasets_data.append(np.mean(f1_lst))
+                    datasets_data.append(np.mean(time_pred))
+                    datasets_data.append(np.mean(total_ins))
+                    log.info('---------------------------------------------') 
+                    log.info('data size '+str(d_size)+' data set '+dataset_name) 
+                    log.info(acc_lst)
+                    log.info(f1_lst)
+                    log.info(time_pred)
+                    log.info(total_ins)
+                    log.info('---------------------------------------------')                    
+                all_data.append(datasets_data)
+            result[ml_key] = all_data
+        pickle.dump(result, open('result.obj', 'wb'))
+        self.report_all(result)
+          
+        
 def mainCmp():
+    print ' ---------- start cmp -------'
     obj = CompareProcess()
     obj.process()
+    print ' ---------- end cmp -------'
     
 if __name__ == '__main__':
-    mainCmp()    
+    mainCmp()
--- dataset_loader.py
+++ dataset_loader.py
@@ -1,17 +1,32 @@
 import numpy as np
 
 class DataSetLoader(object):
+    dataset_name = ['adult', 'heart', 'letter', 'austra', 'german']
 
     def __init__(self):
         pass
     
     def template_load(self, file_name):
-        return np.array(np.genfromtxt(file_name, dtype=int, delimiter=',', autostrip=True))
+        return np.array(np.genfromtxt(file_name, dtype=int, delimiter=','))
+    
+    def template_load_float(self, file_name):
+        return np.genfromtxt(file_name, dtype=float, delimiter=',')
     
     def reshape_y(self, y_train):
         ret = y_train.reshape(1, len(y_train))
         return ret[0]
-            
+    
+    def reshape_y_convert(self, y_train):
+        ret = y_train.reshape(1, len(y_train))
+        return ret[0].astype(int)
+        
+    def get_y_last_convert(self, data):
+        attr = len(data[0])
+        r = range(0, attr - 2)
+        x = data[:, r]
+        y = self.reshape_y_convert(data[:, [attr - 1]])
+        return x, y
+    
     def get_y_last(self, data):
         attr = len(data[0])
         r = range(0, attr - 2)
@@ -30,8 +45,8 @@ class DataSetLoader(object):
         adult = self.template_load('data/binary/adult.data')
         x_adult, y_adult = self.get_y_last(adult)
         
-        heart = self.template_load('data/binary/HeartDisease.data')
-        x_heart, y_heart = self.get_y_last(heart)
+        heart = self.template_load_float('data/binary/HeartDisease.data')
+        x_heart, y_heart = self.get_y_last_convert(heart)
         
         letter = self.template_load('data/binary/letter.p2.data')
         x_letter, y_letter = self.get_y_first(letter)
@@ -39,13 +54,13 @@ class DataSetLoader(object):
         austra = self.template_load('data/statlog/australian.data')
         x_aus, y_aus = self.get_y_last(austra)
         
-        german = self.template_load('data/statlog/australian.data')
-        x_ger, y_ger = self.get_y_last(german)
+        german = self.template_load('data/statlog/german.data')
+        x_ger, y_ger = self.get_y_last_convert(german)
         
         stat = self.template_load('data/statlog/sat.data')
         x_stat, y_stat = self.get_y_last(stat)
         
-        seg = self.template_load('data/statlog/segment.data')
+        seg = self.template_load_float('data/statlog/segment.data')
         x_seg, y_seg = self.get_y_last(seg)
         
         shuttle = self.template_load('data/statlog/shuttle.data')
@@ -54,17 +69,24 @@ class DataSetLoader(object):
         vechicle = self.template_load('data/statlog/vehicle.data')
         x_vechicle, y_vechicle = self.get_y_last(vechicle)
         
-        return {'adult':[x_adult, y_adult]}
+#         return {self.dataset_name[0]:[x_adult, y_adult]}
+        return {self.dataset_name[0]:[x_adult, y_adult],
+                self.dataset_name[1]:[x_heart, y_heart],
+                self.dataset_name[2]:[x_letter, y_letter],
+                self.dataset_name[3]:[x_aus, y_aus],
+                self.dataset_name[4]:[x_ger, y_ger]}
+        
 #         return {'adult':[x_adult, y_adult],
 #                 'heart':[x_heart, y_heart],
 #                 'letter':[x_letter, y_letter],
 #                 'austra':[x_aus, y_aus],
 #                 'german':[x_ger, y_ger],
 #                 'stat':[x_stat, y_stat],
-#                 'seg':[x_seg, y_seg],
+# #                 'seg':[x_seg, y_seg],
 #                 'shuttle':[x_shutt, y_shutt],
 #                 'vechicle':[x_vechicle, y_vechicle]}
-    
-if __name__ == '__main__':
-    obj = DataSetLoader()
-    print obj.loadData()['vechicle'][1]
+
+#     
+# if __name__ == '__main__':
+#     obj = DataSetLoader()
+#     print obj.loadData()['vechicle'][1]
