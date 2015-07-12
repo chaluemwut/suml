f129d4a80a541186575bd7093896fc03593ae72d
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-07-13 00:48:09 +0700

    change config

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ Created on Jul 12, 2015
 
 class Config(object):
 #   production
-#     display_console = False
-#     base_estimation = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-#     reperating_loop = 50
+    display_console = False
+    base_estimation = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    reperating_loop = 50
 
 #   Test
-    display_console = True
-    base_estimation = [2, 4]
-    reperating_loop = 2
+#     display_console = True
+#     base_estimation = [2, 4]
+#     reperating_loop = 2
 
 # common
     ml_name = ['bagging', 'boosted', 'randomforest', 'nb', 'knn', 'decsiontree', 'svm']
