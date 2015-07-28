bfcd6f0b0d20fd36c92f6a0869222090c363a50e
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-07-28 15:30:27 +0700

    fix missing

--- missing_attribute.py
+++ missing_attribute.py
@@ -23,9 +23,9 @@ class MissingAttribute(object):
     def __init__(self, ml_name):
         self.ml_name = ml_name
         log_file = LogFile()
-        self.log = log_file.get_log(self.ml_name+'_data', self.ml_name+'_data.log', Config.display_console)
-        self.log_debug = log_file.get_log(self.ml_name+'_debug', self.ml_name+'_debug.log', Config.display_console)
-        self.log_error = log_file.get_log(self.ml_name+'_error', self.ml_name+'_error.err', Config.display_console)
+        self.log = log_file.get_log(self.ml_name + '_data', self.ml_name + '_data.log', Config.display_console)
+        self.log_debug = log_file.get_log(self.ml_name + '_debug', self.ml_name + '_debug.log', Config.display_console)
+        self.log_error = log_file.get_log(self.ml_name + '_error', self.ml_name + '_error.err', Config.display_console)
    
     def gen_ml_lst(self):
         random_lst = []
@@ -102,6 +102,61 @@ class MissingAttribute(object):
         loader = DataSetLoader()
         lst = loader.loadData()
         return lst        
+  
+    def report_all(self, result):
+        perform_result = []
+        time_result = []
+        for key, value in result.iteritems():
+            all_data = value
+            all_data = np.array(all_data)
+            acc25 = np.mean(all_data[:, 0])
+            f1_25 = np.mean(all_data[:, 1])
+            time25 = np.mean(all_data[:, 2])
+            total_ins25 = np.mean(all_data[:, 3])
+            acc50 = np.mean(all_data[:, 4])
+            f1_50 = np.mean(all_data[:, 5])
+            time50 = np.mean(all_data[:, 6])
+            total_ins50 = np.mean(all_data[:, 7])
+            acc75 = np.mean(all_data[:, 8])
+            f1_75 = np.mean(all_data[:, 9])
+            time75 = np.mean(all_data[:, 10])
+            total_ins75 = np.mean(all_data[:, 11])
+            perform_result.append([key, acc25, acc50, acc75, f1_25, f1_50, f1_75])
+            time_result.append([key, time25, total_ins25, time50, total_ins50, time75, total_ins75])
+        self.log.info('---------- ml report ----------')
+        self.log.info(tabulate(perform_result, headers=('ml name', 'acc 25', 'acc 50', 'acc 75', 'f1 25', 'f1 50', 'f1 75')))
+        self.log.info('---------- time report --------')
+        self.log.info(tabulate(time_result, headers=('ml name', 'time 25', 'ins 25', 'time 50', 'ins 50', 'time 75', 'ins 75')))
+        self.log.info(self.report_by_dataset_v1(result))
+            
+    def report_by_dataset_v1(self, result):
+        self.log_debug.info('report by dataset')
+        result_lst = []
+        for i in range(0, len(self.dataset_name_lst)):
+            ml_result = []
+            datasets_data = result[self.ml_name][i]
+            acc25 = datasets_data[0]
+            f1_25 = datasets_data[1]
+            time25 = datasets_data[2]
+            total_ins25 = datasets_data[3]
+            acc50 = datasets_data[4]
+            f1_50 = datasets_data[5]
+            time50 = datasets_data[6]
+            total_ins50 = datasets_data[7]
+            acc75 = datasets_data[8]
+            f1_75 = datasets_data[9]
+            time75 = datasets_data[10]
+            total_ins25 = datasets_data[11] 
+            ml_result.append(self.ml_name)                               
+            ml_result.append(self.dataset_name_lst[i])
+            ml_result.append(acc25)
+            ml_result.append(acc50)
+            ml_result.append(acc75)
+            ml_result.append(f1_25)
+            ml_result.append(f1_50)
+            ml_result.append(f1_75)
+            result_lst.append(ml_result)
+        return tabulate(result_lst, ('ml name', 'data set', 'acc 25', 'acc 50', 'acc 75', 'f1 25', 'f1 50', 'f1 75'))
        
     def process(self):
         result = {}
@@ -116,9 +171,9 @@ class MissingAttribute(object):
             data_value = dataset_lst[data_set_name]
             x_data = data_value[0]
             y_data = data_value[1]
-            print 'before************** ',x_data[0]
+            print 'before************** ', x_data[0]
             x_data, y_data = self.remove_by_chi2_process(x_data, y_data)
-            print 'after****************',x_data[0]
+            print 'after****************', x_data[0]
             dataset_map = {}
             datasets_data = []            
             for data_size_value in self.data_size:
@@ -157,6 +212,25 @@ class MissingAttribute(object):
                     precision_lst.append(pre_score)
                     recall_lst.append(recall)
                     self.log_debug.info('------------- end loop -----')
+                datasets_data.append(np.mean(acc_lst))
+                datasets_data.append(np.mean(f1_lst))
+                datasets_data.append(np.mean(time_pred))
+                datasets_data.append(np.mean(total_ins))
+                self.log.info('---------------------------------------------') 
+                self.log.info('data size ' + str(data_size_value) + ' data set ' + data_set_name) 
+                self.log.info(acc_lst)
+                self.log.info(f1_lst)
+                self.log.info(time_pred)
+                self.log.info(total_ins)
+                self.log.info('---------------------------------------------')
+                self.log_debug.info('*********** end size')                    
+            all_data.append(datasets_data)
+            dataset_map[data_set_name] = all_data
+            self.log_debug.info('******* end data set')
+        result[self.ml_name] = all_data
+        self.log_debug.info('************ end ml')
+        pickle.dump(result, open(self.ml_name + '_result.obj', 'wb'))
+        self.report_all(result)                    
                     
 if __name__ == '__main__':
     ml_key = sys.argv[1]
