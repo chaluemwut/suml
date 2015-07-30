e3887ecf3d1429ccd337f9134ed41386161c69d4
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-07-30 22:08:30 +0700

    change data set

--- svmrunner.sh
+++ svmrunner.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-python svm_manual.py adult &
-python svm_manual.py heart &
-python svm_manual.py letter &
-python svm_manual.py austra &
-python svm_manual.py german &
-python svm_manual.py sat &
 python svm_manual.py segment &
 python svm_manual.py shuttle &
-python svm_manual.py vehicle &
