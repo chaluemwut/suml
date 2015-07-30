dc7e802ddb8b0ad4634f8ca4872178bde0659535
chaluemwut_noyunsan <chaluemwut@gmail.com>
2015-07-30 22:19:47 +0700

    missing knn

--- missing_runner.sh
+++ missing_runner.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-python missing_attribute.py bagging &
-python missing_attribute.py boosted &
-python missing_attribute.py randomforest &
-python missing_attribute.py nb &
-python missing_attribute.py decsiontree &
+python missing_attribute.py knn &
